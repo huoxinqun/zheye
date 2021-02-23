0f3af8fc455370f2e75c15dd2cdb22428b9ee1a3
huoxinqun <1287324114@qq.com>
2021-02-23 10:59:56 +0800

    专栏详情，axios data,async封装

--- src/store.ts
+++ src/store.ts
@@ -1,41 +1,47 @@
 import axios from 'axios'
-import { createStore } from 'vuex'
+import { createStore,Commit } from 'vuex'
 
-interface UserProps {
+export interface UserProps {
     isLogin: boolean;
     name?: string;
     id?: number
 }
 export interface ColumnProps {
-  _id:number;
+  _id:string;
   title:string;
   avatar ?: ImageProps;
   description ?: string;
 }
-interface ImageProps {
+export interface ImageProps {
   _id ?: string;
   url ?: string;
   createdAt ?: string;
 }
 export interface PostProps {
-  id:string;
+  _id:string;
   title:string;
+  excerpt ?: string;
   content:string;
-  image?: string;
+  image?: ImageProps;
   createdAt : string;
-  columnId:　number;   
+  column:string;   
 }
 export interface GlobalDataProps {
   user: UserProps;
   columns:ColumnProps[];
-  // posts:PostProps[];
+  posts:PostProps[];
   
+}
+//定义get 公共async方法
+const getAndCommit = async (url:string,mutationName:string,commit:Commit) => {
+  const { data } = await axios.get(url)
+  commit(mutationName,data)
 }
 const store = createStore<GlobalDataProps>({
     state: {
       user: { isLogin: false },
       columns: [],
-      // posts: testPosts
+      posts: []
     },
     mutations: {
       login(state) {
@@ -43,17 +49,44 @@ const store = createStore<GlobalDataProps>({
       },
       fetchColumns(state,rawData){
         state.columns = rawData.data.list
+      },
+      fetchColumn(state,rawData){
+        state.columns = [rawData.data]
+      },
+      fetchPosts(state,rawData){
+        state.posts = rawData.data.list
       }
       // createPost(state,newPost) {
       //   state.posts.push(newPost)
       // }
     },
     actions:{
-      fetchColumns(context) {
-        axios.get('/columns').then(resp =>{
-          console.log(resp.data)
-           context.commit('fetchColumns',resp.data)
-        })
+      // 获取首页文章列表
+      // async fetchColumns(context) {
+      //   axios.get('/columns').then(resp =>{
+      //      context.commit('fetchColumns',resp.data)
+      //   })
+      // },
+     fetchColumns({ commit }) {
+       getAndCommit('/columns','fetchColumns',commit)
+        // const { data } = await axios.get('/columns')  
+        // commit('fetchColumns',data)
+      },
+      // 获得一个专栏详情
+      async fetchColumn({ commit },cid) {
+        getAndCommit(`/columns/${cid}`,'fetchColumn',commit)
+      },
+      // 获取专栏的文章列表
+      async fetchPosts({ commit },cid) {
+        getAndCommit(`/columns/${cid}/posts`,'fetchPosts',commit)       
+      },
+    },
+    getters :{
+      getColumnById:(state) => (id:string) => {
+        return state.columns.find(c => c._id === id)
+      },
+      getPostsByCid:(state) => (cid:string) => {
+        return state.posts.filter(post => post.column === cid)
       }
     }
   })
